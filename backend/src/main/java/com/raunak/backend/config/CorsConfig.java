a0857e20b552c7c7b4b0e214b821882715bbe0d4
Raunak <raunakzyx2@gmail.com>
2026-07-31 22:03:49 +0530

    extension id(web store) update cors

--- backend/src/main/java/com/raunak/backend/config/CorsConfig.java
+++ backend/src/main/java/com/raunak/backend/config/CorsConfig.java
@@ -22,7 +22,7 @@ public class CorsConfig {
         configuration.setAllowedOrigins(
                 List.of(
                         frontendUrl,
-                        "chrome-extension://jjldklblhakemhjhjlpcnjaljdajghid"
+                        "chrome-extension://cnfpjafdoegkfnbhnejcjdbppddccppg"
                 )
         );
 
