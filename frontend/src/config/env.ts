a9e5a445306c612b3927d07babadab19716c8053
Raunak <raunakzyx2@gmail.com>
2026-08-25 13:52:30 +0530

    prod env update

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -1,13 +1,18 @@
-const useProduction = process.env.NEXT_PUBLIC_USE_PRODUCTION === "true";
+const useProduction =
+  process.env.NEXT_PUBLIC_USE_PRODUCTION === "true" ||
+  process.env.NODE_ENV === "production";
+
 export const env = {
   isProd: useProduction,
 
   backendUrl: useProduction
-    ? process.env.NEXT_PUBLIC_PROD_BACKEND!
-    : process.env.NEXT_PUBLIC_LOCAL_BACKEND!,
+    ? process.env.NEXT_PUBLIC_PROD_BACKEND || "https://api.unsheet.in"
+    : process.env.NEXT_PUBLIC_LOCAL_BACKEND || "http://localhost:8080",
 
   frontendUrl: useProduction
-    ? process.env.NEXT_PUBLIC_PROD_FRONTEND!
-    : process.env.NEXT_PUBLIC_LOCAL_FRONTEND!,
-  EXTENSION_ID: process.env.NEXT_PUBLIC_EXTENSION_ID!,
+    ? process.env.NEXT_PUBLIC_PROD_FRONTEND || "https://unsheet.in"
+    : process.env.NEXT_PUBLIC_LOCAL_FRONTEND || "http://localhost:3000",
+
+  EXTENSION_ID:
+    process.env.NEXT_PUBLIC_EXTENSION_ID || "cnfpjafdoegkfnbhnejcjdbppddccppg",
 };
